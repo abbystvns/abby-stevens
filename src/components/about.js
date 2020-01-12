07dcb9f338e1d4c2188b4f4e97c2de4a1d947902
Abby Stevens <abby.e.stevens@gmail.com>
2020-01-12 15:33:14 -0600

    update

--- src/components/about.js
+++ src/components/about.js
@@ -10,9 +10,11 @@ export const About = () => {
       <div className="info">
         <h4> About Me </h4>
         <p>
-        Hi! I'm a a third year PhD student in Statistics at <a href="https://stat.uchicago.edu/">UChicago</a>
-        advised by  <a href="https://voices.uchicago.edu/willett/">Becca Willett</a>. I like using data to do good things.
+        Hi! I'm a a third year PhD student in Statistics at <a href="https://stat.uchicago.edu/">UChicago</a> 
+        advised by  <a href="https://voices.uchicago.edu/willett/">Becca Willett</a>. I like doing good things with data, which currently means developing new methodologies for tackling climate change with machine learning.
 
+        Before graduate school, I worked as a data scientist at <a href="https://www.doximity.com/">Doximity</a> and <a href="https://betaworks.com/">betaworks</a>. I am involed in a number of data science for social good initiaives, including stints with the <a href="https://codeforsanfrancisco.org/projects/the-data-science-working-group/">Data Science Working Group at Code for San Francisco</a> and the <a href="http://www.datasciencepublicpolicy.org/">Center for Data Science and Public Policy</a>.
+        I am currently helping organize the <a href="https://www.widsconference.org/">Women in Data Science Chicago</a>regional event.
           <br /> <br />I can be reached at{" "}
           <a
             href={`mailto:  abbystevens@uchicago.edu`}
