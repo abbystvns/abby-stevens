ff2c7e4201a2d699f95d050da07236db5bbde169
Abby Stevens <abby.e.stevens@gmail.com>
2020-01-12 19:23:22 -0600

    lol

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ export const About = () => {
         <p>
         Hi! I'm a a third year PhD student in Statistics at <a href="https://stat.uchicago.edu/">UChicago</a> advised by  <a href="https://voices.uchicago.edu/willett/">Becca Willett</a>. I like doing good things with data, which currently means developing new methodologies for tackling climate change with machine learning.
 <br /><br />
-        Before grad school, I worked as a data scientist at <a href="https://www.doximity.com/">Doximity</a> and <a href="https://betaworks.com/">betaworks</a>. I have been invovled with a number of data science for social good initiaives, including stints with the <a href="https://codeforsanfrancisco.org/projects/the-data-science-working-group/">Data Science Working Group at Code for San Francisco</a> and the <a href="http://www.datasciencepublicpolicy.org/">Center for Data Science and Public Policy</a>.
+        Before this, I worked as a data scientist at <a href="https://www.doximity.com/">Doximity</a> and <a href="https://betaworks.com/">betaworks</a>. I graduated with a math degreee from <a href="https://www.grinnell.edu/academics/majors-concentrations/math-stats">Grinnell College</a> in 2014. I have been invovled with a number of data science for social good initiaives, including stints with the <a href="https://codeforsanfrancisco.org/projects/the-data-science-working-group/">Data Science Working Group at Code for San Francisco</a> and the <a href="http://www.datasciencepublicpolicy.org/">Center for Data Science and Public Policy</a>.
         I am currently helping organize the <a href="https://www.widsconference.org/">Women in Data Science Chicago</a> regional event.
          
           <br /> <br />I can be reached at{" "}
@@ -23,10 +23,8 @@ export const About = () => {
           >
             abbystevens@uchicago.edu
           </a>
-          <br />(<a href="https://github.com/abbystvns">github</a>,  
-           <a href="https://www.linkedin.com/in/abbyestevens/">linkedin</a>, 
-           <a href="https://twitter.com/abbystvns">twitter</a>, and 
-           <a href="https://www.instagram.com/abbbyst/">instagram</a>.
+          <br />
+          <a href="https://github.com/abbystvns">github</a>, <a href="https://www.linkedin.com/in/abbyestevens/">linkedin</a>, <a href="https://twitter.com/abbystvns">twitter</a>, <a href="https://www.instagram.com/abbbyst/">instagram</a>
         </p>
       </div>
     </div>
--- src/pages/research.js
+++ src/pages/research.js
@@ -8,40 +8,48 @@ const Research = () => (
     <SEO title="Research" />
     <div className="main">
       <div className="container">
-        <h3>Research Interests</h3>
+        <h4>Research Interests</h4>
         <div style={{ paddingLeft: `2rem` }}>
           <ul>
-            <li></li>
-            <li>Math</li>
-            <li>Math</li>
-            <li>Math</li>
+            <li>High dimensional statistics</li>
+            <li>Applied statistics and machine learning</li>
+            <li>Fairness and ethics in data science</li>
+            <li>Machine learning for climate science</li>
           </ul>
         </div>
-        <h3 style={{ marginTop: `2rem` }}>Publications & Presentations</h3>
+        <h4 style={{ marginTop: `2rem` }}>Publications</h4>
+        
         <p>
-          “Leveraging large ensemble climate simulations and graph-guided
-          regularization for improving seasonal hydroclimatic forecasting,”
-          <i>Large Ensembles Workshop</i>, Boulder, CO, July 2019.{" "}
-          <i>(poster)</i>
+          P. Saleiro, B. Kuester, L. Hinkson, J. London, <strong>A. Stevens</strong>, A.
+          Anisfeld, K. Rodolfa, and R. Ghani. <i>Aequitas: A Bias and Fairness
+          Audit Toolkit</i>. November 2018. 
+          (<a href="http://www.datasciencepublicpolicy.org/projects/aequitas/">tool</a>, <a href="https://arxiv.org/pdf/1811.05577.pdf">arxiv</a>)
+          <br />
+        </p>
+        <h4 style={{ marginTop: `2rem` }}>Conference Presentations and Posters</h4>
+        <p>
+
         </p>
         <p>
-          “Leveraging large ensemble climate simulations and graph-guided
-          regularization for improving seasonal hydroclimatic forecasting,”{" "}
+          Leveraging large ensemble climate simulations and graph-guided
+          regularization for improving seasonal hydroclimatic forecasting,
+          <i>Large Ensembles Workshop</i>, Boulder, CO, July 2019.{" "} (<a href="https://usclivar.org/sites/default/files/meetings/2019/posters/StevensPoster.pdf">(poster)</a>
+        </p>
+        <p>
+          Leveraging large ensemble climate simulations and graph-guided
+          regularization for improving seasonal hydroclimatic forecasting,{" "}
           <i>Midwest Machine Learning Symposium</i>, Madison, WI, June 2019.
           (poster)
         </p>
+
+
+        <h4 style={{ marginTop: `2rem` }}>Talks</h4>
         <p>
           “Graph Total Variation for Seasonal Forecasting,”{" "}
           <i>Computational and Applied Mathematics Student Seminar</i>, Chicago,
           IL, April 2019.
         </p>
-        <p>
-          P. Saleiro, B. Kuester, L. Hinkson, J. London, A. Stevens, A.
-          Anisfeld, K. Rodolfa, and R. Ghani. "Aequitas: A Bias and Fairness
-          Audit Toolkit,"
-          <i>eprint arXiv:1811.05577</i>, November 2018.
-          <br />
-        </p>
+        </h4>
       </div>
     </div>
     <BackButton />
