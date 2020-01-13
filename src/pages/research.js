c79b19fbc2958b551946961e47ee4068184f1c12
Abby Stevens <abby.e.stevens@gmail.com>
2020-01-12 21:04:54 -0600

    research

--- src/pages/research.js
+++ src/pages/research.js
@@ -18,7 +18,12 @@ const Research = () => (
           </ul>
         </div>
         <h4 style={{ marginTop: `2rem` }}>Publications</h4>
-
+        <p> <strong> A. Stevens</strong>, R. Willett, A. Mamalakis, E. Foufoula-Georgiou, J. Randerson, 
+        P. Smyth, S. Wright and A. Tejedor. Graph-guided regularization for improved seasonal forecasting. In
+        Brajard, J., Charantonis, A., Chen, C., & Runge, J. (Eds.). (2019). Proceedings of the
+ 9th International Workshop on Climate Informatics: CI 2019 (No. NCAR/TN-
+ 561+PROC). doi:10.5065/y82j-f154.
+        </p>
         <p>
           P. Saleiro, B. Kuester, L. Hinkson, J. London, <strong>A. Stevens</strong>, A.
           Anisfeld, K. Rodolfa, and R. Ghani. <i>Aequitas: A Bias and Fairness
@@ -28,12 +33,16 @@ const Research = () => (
         </p>
         <h4 style={{ marginTop: `2rem` }}>Conference Presentations and Posters</h4>
         <p>
-
+        Graph-guided regularization for improved forecasting of Southwestern US winter precipitation, <i> AGU </i>, San Francisco, CA, December 2019.
+        </p>
+        <p>
+        Graph-guided regularization for improved seasonal forecasting, <i> Workshop on Climate Informatics</i>, Paris, France, September 2019. 
+        (<a href="https://sites.google.com/view/climateinformatics2019/posters">poster</a>)
         </p>
         <p>
           Leveraging large ensemble climate simulations and graph-guided
           regularization for improving seasonal hydroclimatic forecasting,
-          <i>Large Ensembles Workshop</i>, Boulder, CO, July 2019.{" "} (<a href="https://usclivar.org/sites/default/files/meetings/2019/posters/StevensPoster.pdf">(poster)</a>
+          <i>Large Ensembles Workshop</i>, Boulder, CO, July 2019.{" "} (<a href="https://usclivar.org/sites/default/files/meetings/2019/posters/StevensPoster.pdf">poster</a>)
         </p>
         <p>
           Leveraging large ensemble climate simulations and graph-guided
